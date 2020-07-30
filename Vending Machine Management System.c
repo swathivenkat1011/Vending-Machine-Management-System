076eb9c93356d608e82da1820cfd941eb834b6b7
swathivenkat1011 <swathivenkat1011@gmail.com>
2020-07-30 18:23:30 +0530

    Added initial commit with menu display functionality

---
+++ Vending Machine Management System.c
@@ -0,0 +1,23 @@
+//Vending Machine Management System
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int cost;
+    while(1)
+    {
+        printf("\t\tVENDING MACHINE MANAGEMENT SYSTEM\n\n");
+        printf("\t--------------------------------------------------\n");
+        printf("\t|    SNACKS    |  PRICE  |    DRINKS    |  PRICE  |\n");
+        printf("\t--------------------------------------------------\n");
+        printf("\t| Potato Chips |   10    | Coke         |   7     |\n");
+        printf("\t| Cracker      |   12    | Lemonade     |   10    |\n");
+        printf("\t| PopCorn      |    5    | Orange Juice |   11    |\n");
+        printf("\t| Cashew       |   15    | Milkshade    |   15    |\n");
+        printf("\t--------------------------------------------------\n");
+        break;
+    }
+
+return 0;
+}
+
